6b7734959af9f6cb2f6ac7f028a0065b085a499c
Ammar Hussain <syedhussainammar12@gmail.com>
2023-05-24 09:06:59 -0400

    Clean up of program + non-functional slider
    Shortened and omitted comments that were unnecessarily long - added a non-functional slider that is a W.I.P through LVGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include "main.h"
 #include "PIDController.h"
 
+// creation of button and labels within LVGL library
 lv_obj_t * myButton;
 lv_obj_t * myButtonLabel;
 lv_obj_t * myLabel;
-lv_obj_t * mySlider;
+// lv_obj_t * sliderLabel;
+// lv_obj_t * mySlider;
 
+// separate styles of button when idle and pressed
 lv_style_t myButtonStyleREL; // idle state
 lv_style_t myButtonStylePR; // pressed state
-lv_style_t myBarStyle;
+// lv_style_t myBarStyle;
 
+// left side motor group
+pros::Motor leftMotorBack(3);
+pros::Motor leftMotorFront(8);
+pros::Motor_Group leftDriveSmart({leftMotorBack,leftMotorFront});
+	
+// right side motor group
+pros::Motor rightMotorBack(4);
+pros::Motor rightMotorFront(7);
+pros::Motor_Group rightDriveSmart({rightMotorFront, rightMotorBack});
 
+// sets
 static lv_res_t btn_click_action(lv_obj_t * btn)
 {
     uint8_t id = lv_obj_get_free_num(btn); //id usefull when there are multiple buttons
@@ -18,19 +31,60 @@ static lv_res_t btn_click_action(lv_obj_t * btn)
     if(id == 0)
     {
         char buffer[100];
-		sprintf(buffer, "Button was clicked %ims from start of program", pros::millis());
+		sprintf(buffer, "GET TROLLLLLLED");
 		lv_label_set_text(myLabel, buffer);
+		while(1) {
+			leftDriveSmart.move_voltage(12000);
+			rightDriveSmart.move_voltage(12000);
+		}
     }
 
     return LV_RES_OK;
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+
+
+////////////////////////////
+// NOT WORKING SLIDER BAR //
+////////////////////////////
+
+/* extern "C" {
+	static lv_obj_t * label;
+
+static void slider_event_cb(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target(e);
+
+    /*Refresh the text
+    lv_label_set_text_fmt(label, "%"LV_PRId32, lv_slider_get_value(slider));
+    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider
+}
+
+/*
+ * Create a slider and write its value on a label.
+ 
+void lv_example_get_started_4(void)
+{
+    /*Create a slider in the center of the display
+    lv_obj_t * slider = lv_slider_create(lv_scr_act());
+    lv_obj_set_width(slider, 200);                          /*Set the width
+    lv_obj_center(slider);                                  /*Align to the center of the parent (screen)
+    lv_obj_add_event(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function
+
+    /*Create a label above the slider
+    label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "0");
+    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider
+}
+*/
+///////////////////////////////////
+// END OF NOT WORKING SLIDER BAR //
+///////////////////////////////////
+
+
+
+
+// Runs initialization code. This occurs as soon as the program is started.
 void initialize() {
 	
 
@@ -46,21 +100,23 @@ void initialize() {
     myButtonStylePR.body.radius = 0;
     myButtonStylePR.text.color = LV_COLOR_MAKE(255, 255, 255);
 
-    myButton = lv_btn_create(lv_scr_act(), NULL); //create button, lv_scr_act() is deafult screen object
+    myButton = lv_btn_create(lv_scr_act(), NULL); //create button, lv_scr_act() is default screen object
     lv_obj_set_free_num(myButton, 0); //set button is to 0
-    lv_btn_set_action(myButton, LV_BTN_ACTION_CLICK, btn_click_action); //set function to be called on button click
-    lv_btn_set_style(myButton, LV_BTN_STYLE_REL, &myButtonStyleREL); //set the relesed style
-    lv_btn_set_style(myButton, LV_BTN_STYLE_PR, &myButtonStylePR); //set the pressed style
-    lv_obj_set_size(myButton, 200, 50); //set the button size
-    lv_obj_align(myButton, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 10); //set the position to top mid
+    lv_btn_set_action(myButton, LV_BTN_ACTION_CLICK, btn_click_action); // sets function to be called on button click
+    lv_btn_set_style(myButton, LV_BTN_STYLE_REL, &myButtonStyleREL); // sets the released style
+    lv_btn_set_style(myButton, LV_BTN_STYLE_PR, &myButtonStylePR); // sets the pressed style
+    lv_obj_set_size(myButton, 200, 50); // sets the button size
+    lv_obj_align(myButton, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 10); // sets the position to top left
 
-    myButtonLabel = lv_label_create(myButton, NULL); //create label and puts it inside of the button
-    lv_label_set_text(myButtonLabel, "Click the Button"); //sets label text
+    myButtonLabel = lv_label_create(myButton, NULL); // create label and puts it inside of the button
+    lv_label_set_text(myButtonLabel, "FREE MONEY!!!!"); // sets label text
 
-    myLabel = lv_label_create(lv_scr_act(), NULL); //create label and puts it on the screen
-    lv_label_set_text(myLabel, "Button has not been clicked yet"); //sets label text
-    lv_obj_align(myLabel, NULL, LV_ALIGN_IN_LEFT_MID, 10, -40); //set the position to center
+    myLabel = lv_label_create(lv_scr_act(), NULL); // create label and puts it on the screen
+    lv_label_set_text(myLabel, "CLICK BUTTON FOR FREE MOOLA"); // sets label text
+    lv_obj_align(myLabel, NULL, LV_ALIGN_IN_LEFT_MID, 10, -40); // sets the position to center
 }
+
+
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
@@ -68,6 +124,8 @@ void initialize() {
  * the robot is enabled, this task will exit.
  */
 void disabled() {}
+
+
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -80,37 +138,15 @@ void disabled() {}
  */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
+
+
+
+// Runs the user autonomous code when the Competition Field Switch is enabled to autonomous.
 void autonomous() {}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 
 
-
-
-// driving control function
+// Driver Control function
 void opcontrol() {
 
 	// left side motor group
