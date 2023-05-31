5764c8030f3eaf9480c4cfc956c5d0cc649bd8af
mochi48 <127371243+mochi48@users.noreply.github.com>
2023-05-30 20:28:14 -0400

    Basic (Unconfigured) Project Layout

--- ammar-pid-&-joystick/include/robot-config.h
+++ ammar-pid-&-joystick/include/robot-config.h
@@ -2,16 +2,9 @@ using namespace vex;
 
 extern brain Brain;
 
-// VEXcode devices
-extern controller Controller1;
-extern motor leftMotorA;
-extern motor leftMotorB;
-extern motor rightMotorA;
-extern motor rightMotorB;
-
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
+ *
  * This should be called at the start of your int main function.
  */
-void  vexcodeInit( void );
+void vexcodeInit(void);
