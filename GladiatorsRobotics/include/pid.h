53c2b1c8aa12697179c37edfeae015d7f5e96394
mochi48 <127371243+mochi4852@users.noreply.github.com>
2023-11-08 01:02:26 -0500

    PID/Program Changes
    1. Completely changed how PID functions. Rather than being one controller that is reused, it now creates a controller per function call, via the reuse of motors/groups.
    
    2. Restructured the entire program in terms of efficiency. Some things may be buggy! Needs to be tested!
    
    3. Created a project called "GladiatorsRobotics," used as placeholder for *DriverControl* and *Autonomous*. It does NOT include driverSkills. Everything else, including old projects, have been moved into a folder called PRE_CLEANUP.
    
    For any questions feel free to ask me anything.
    - Jonathan

---
+++ GladiatorsRobotics/include/pid.h
@@ -0,0 +1,105 @@
+#include "vex.h"
+
+// create PID controller class
+class PIDController {
+
+  // class private variables
+  private:
+    vex::motor* motorA;
+    vex::motor* motorB;
+    vex::motor* motorC;
+    vex::motor* motorD;
+    vex::motor_group* motor_groupA;
+    vex::motor_group* motor_groupB;
+    vex::inertial* drivetrain_intertial;
+  
+  // class public variables
+  public:
+    PIDController(vex::motor* motA, vex::motor* motB, vex::motor* motC, vex::motor* motD, vex::motor_group* motGroupA, vex::motor_group* motGroupB, vex::inertial* driInertial) {
+      motorA = motA;
+      motorB = motB;
+      motorC = motC;
+      motorD = motD;
+      motor_groupA = motGroupA;
+      motor_groupB = motGroupB;
+      drivetrain_intertial = driInertial;
+    }
+
+    // pid movement to go forward
+    void moveLateral(double targetValue, double proportionalGain, double integralGain, double derivativeGain) {
+
+      // repeat until target is reached
+      while (true) {
+
+        // establish accumulative variables
+        double integral;
+        double lastError;
+        double derivative;
+
+        // get average motor positions
+        double motorAveragePosition = (motorA->position(vex::degrees) + motorB->position(vex::degrees) + 
+        motorC->position(vex::degrees) + motorD->position(vex::degrees)) / 4;
+
+        // get error margin
+        double error = targetValue - motorAveragePosition;
+        integral += error;
+        derivative = error - lastError;
+        lastError = error;
+
+        // output value
+        double outputValue = (proportionalGain * error + integralGain * integral + derivativeGain * derivative);
+
+        // movement
+        motor_groupA->spin(vex::directionType::fwd, outputValue, vex::voltageUnits::volt);
+        motor_groupB->spin(vex::directionType::fwd, outputValue, vex::voltageUnits::volt);
+
+        // stop movement and break out of pid if error is less than 1
+        if (error < 1) {
+          motorA->stop(vex::brake);
+          motorB->stop(vex::brake);
+          break;
+        }
+
+        // prevent brain from dying
+        vex::task::sleep(20);
+      }
+    }
+
+    void rotate(double targetValue, double proportionalGain, double integralGain, double derivativeGain) {
+      
+      // repeat until target is reached
+      while (true) {
+
+        // establish accumulative variables
+        double integral;
+        double lastError;
+        double derivative;
+
+        // get average motor positions
+        double currentRotation = drivetrain_intertial->heading(vex::degrees);
+
+        // get error margin
+        double error = targetValue - currentRotation;
+        integral += error;
+        derivative = error - lastError;
+        lastError = error;
+
+        // output value
+        double outputValue = (proportionalGain * error + integralGain * integral + derivativeGain * derivative);
+
+        // movement
+        motor_groupA->spin(vex::directionType::fwd, outputValue, vex::voltageUnits::volt);
+        motor_groupB->spin(vex::directionType::rev, outputValue, vex::voltageUnits::volt);
+
+        // stop movement and break out of pid if error is less than 1
+        if (error < 1) {
+          motorA->stop(vex::brake);
+          motorB->stop(vex::brake);
+          break;
+        }
+
+        // prevent brain from dying
+        vex::task::sleep(20);
+      }
+    }
+};
---
+++ GladiatorsRobotics/src/main.cpp
@@ -0,0 +1,201 @@
+// import all configurations and libraries
+#include "config.h"
+
+// configure program for competition
+using namespace vex;
+competition Competition;
+
+// converts distance to rotations
+int distanceToTheta (int distance){
+  double gearRatio = 1.67;
+  double wheelDiameter = 4.25;
+  double wheelCircumference = wheelDiameter * M_PI;
+  double theta = (distance*360) / (wheelCircumference *gearRatio);
+  return theta;
+}
+
+// resets all motors and sensors
+void resetMotorValues() {
+  leftBackMotor.setPosition(0, degrees);
+  leftFrontMotor.setPosition(0, degrees);
+  rightBackMotor.setPosition(0, degrees);
+  rightFrontMotor.setPosition(0, degrees); 
+  DrivetrainInertial.setHeading(0, degrees);
+}
+
+// https://www.desmos.com/calculator/sdcgzah5ya
+int curveJoystick(bool red, int input, double t){ 
+  int val = 0;
+
+  // less sensitive at 20-70, more sensitive when controller input is > 80
+  if(red){ 
+    val = (std::exp(t/10)+std::exp((std::abs(input)-100)/10)*(1-std::exp(-t/10))) * input; 
+  } else {
+
+    // more sensitive at 20-80, more gradual incline as controller input reaches max
+    val = std::exp(((std::abs(input)-100)*20)/1000) * input;
+  }
+  return val;
+}
+
+// determines the joystick positions
+void joystickThreadCallback() {
+  
+  // lateral and turning sensitivity
+  double turningCurve = 30;
+  double forwardCurve = 30;
+
+  // chooses between two turns
+  bool turningRed = false; 
+  bool forwardRed = false;
+  while (true) {
+
+    // calculates joystick curve based on function
+    double turnVal = curveJoystick(turningRed, Controller1.Axis1.position(percent), turningCurve);
+    double forwardVal = curveJoystick(forwardRed, Controller1.Axis3.position(percent), forwardCurve);
+
+    // max voltage on motors is twelve volts; this converts joystick input to volts
+    double turnVolts = turnVal * 0.12; 
+    double forwardVolts = forwardVal * 0.12; 
+    
+    // Applies the voltages to the motors.
+    LeftDriveSmart.spin(forward, forwardVolts + turnVolts, voltageUnits::volt); 
+    RightDriveSmart.spin(forward, forwardVolts - turnVolts, voltageUnits::volt);
+
+    // Forces the thread for 10 milliseconds to sleep to prevent it from using all of the CPU's resources.
+    this_thread::sleep_for(20);
+  }
+}
+
+// pneumatics control code
+void pneumaticsControlCallback() {
+
+  // initial state of pneumatics
+  bool pneumaticsActive = true;
+  while (true) {
+
+    // toggles pneumaticsActive variable and opens pneumatics
+    if (Controller1.ButtonL2.pressing() && pneumaticsActive) {
+      pneumaticsActive = false;
+      pneuCylinLeft.set(true);
+      pneuCylinRight.set(true);
+      this_thread::sleep_for(1000);
+    }
+
+    // toggles pneumaticsActive variable and closes pneumatics
+    else if (Controller1.ButtonL2.pressing()) {
+      pneumaticsActive = true;
+      pneuCylinLeft.set(false);
+      pneuCylinRight.set(false);      
+      this_thread::sleep_for(1000);
+    }
+
+    // prevent robot from throttling
+    this_thread::sleep_for(20);
+  }
+}
+
+// WIP FUNCTION - SEMI FUNCTIONAL!
+void toggleCatapult() {
+
+  // initial toggle state
+  bool cataMotorSpin = false;
+  while (true) {
+    if (cataMotorSpin) {
+      catapult.spin(forward);
+    } 
+    else {
+      
+      // run catapult until distance < 80mm
+      while (disSense.objectDistance(mm) >=  80) {
+        catapult.spin(forward);
+      }
+      catapult.stop();
+    }
+
+    // if R2 is pressed toggle spinning status of catapult
+    if (Controller1.ButtonR2.pressing()) {
+      cataMotorSpin = !cataMotorSpin;
+      if (cataMotorSpin) {
+        catapult.spin(forward);
+      } 
+      else {
+        //while (disSense.objectDistance(mm) >=  80) {
+        //catapult.spin(forward);
+      }
+      catapult.stop();
+    }
+    this_thread::sleep_for(200); 
+  }
+  this_thread::sleep_for(20);
+}
+
+// WIP FUNCTION - NON FUNCTIONAL!
+void holdCatapult() { 
+  /*
+  while(true) { 
+    if(Controller1.ButtonR1.pressing()) {
+      catapultMotor.setVelocity(100, pct);
+      cataSecondMotor.setVelocity(100, pct);
+      catapultMotor.spin(forward);
+      cataSecondMotor.spin(forward);
+    }
+  }
+  this_thread::sleep_for(5);
+  */
+}
+
+// pre-autonomous code; other words for robot before-start settings!
+void pre_auton(void) {
+
+  // configure & setup initial startup elements
+  Drivetrain.setStopping(brake);
+  catapult.setStopping(brake);
+  DrivetrainInertial.calibrate();
+  Drivetrain.setDriveVelocity(100, percent);
+  catapult.setVelocity(100, percent);
+  pneuCylinLeft.set(false);
+  pneuCylinRight.set(false);
+
+  // draw logo on screen
+  Brain.Screen.drawImageFromFile("Robotics Logo - Resized for VEX V5.png", 0, 0);
+}
+
+// autonomous code here
+void autonomous(void) {
+  resetMotorValues();
+  PIDController PID(&leftFrontMotor, &leftBackMotor, &rightFrontMotor, &rightBackMotor, &LeftDriveSmart, &RightDriveSmart, &DrivetrainInertial);
+  double desiredDistance = distanceToTheta(48);
+  PID.moveLateral(desiredDistance, 0.00, 0.00, 1.00);
+  PID.rotate(90, 0.00, 0.00, 0.00);
+}
+
+// user control code here
+void usercontrol(void) {
+
+  // Sets up the multithreading in a while loop that runs forever.
+  thread toggleCylinders = thread(pneumaticsControlCallback);
+  thread toggleCatapultThread = thread(toggleCatapult);
+  thread joystickCurve = thread(joystickThreadCallback);
+
+  // ensure program stays in user control
+  while(true) {
+    wait(20, msec);
+  }
+}
+
+// program main call
+int main() {
+
+  // set up callbacks for autonomous and driver control periods
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Runs the pre-autonomous function.
+  pre_auton();
+
+  // maintains the program running
+  while (true) {
+    wait(100, msec);
+  }
+}
